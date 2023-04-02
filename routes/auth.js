caa4b6d79350a85c233023292cf539194f289888
Joaquin Barrandeguy <joaquinbarrandeguy@gmail.com>
2023-04-02 19:07:41 -0300

    Backend 1.0

---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+/*
+    Rutas de Usuarios / Auth
+    host + /api/auth
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator')
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-compos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+// Defino las rutas y le agrego los controladores (logica)
+
+router.post(
+    '/new', 
+    [ // middlewares
+        check('name', 'El usuario es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'La contraseña es obligatoria').not().isEmpty(),
+        check('sector', 'El sector es obligatorio'),
+        validarCampos
+    ],
+    crearUsuario 
+);
+
+router.post(
+    '/', 
+    [ // middlewares - Validators -
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'La contraseña es obligatoria').not().isEmpty(),
+        validarCampos // custom middleware
+    ],
+    loginUsuario);
+
+router.get('/renew', validarJWT ,revalidarToken );
+
+module.exports = router
