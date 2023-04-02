c529510103ffe662c6e370716a028d16d9e8f42e
Joaquin Barrandeguy <joaquinbarrandeguy@gmail.com>
2023-04-02 19:26:14 -0300

    Backend 1.3

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const express = require('express');
-const dbConnection = require('./database/config');
-const cors = require('cors');
 require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
 
-
-//* Crear el servidor de express
+// Crear el servidor de express
 const app = express();
 
-//*Base de datos
+// Base de datos
 dbConnection();
 
-//* Cors
+// CORS
 app.use(cors())
 
-//* Directorio publico
+// Directorio Público
 app.use( express.static('public') );
 
-//* Lectura y parseo del body (middelware)
+// Lectura y parseo del body
 app.use( express.json() );
 
-//* Rutas
-app.use('/api/auth', require('./routes/auth'));
+// Rutas
+app.use('/api/auth', require('./routes/auth') );
 
 
 //* Escuchar peticiones
